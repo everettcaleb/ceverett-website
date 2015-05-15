fbdcebce8d3e06906de5e677d87aae2c8be91c01
Caleb Everett <everettcaleb95@gmail.com>
2015-05-14 22:28:29 -0400

    Added shim for requirejs and bootstrap

--- wwwroot/lib/main.js
+++ wwwroot/lib/main.js
@@ -3,5 +3,10 @@ requirejs.config({
     paths: {
         jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min',
         bootstrap: '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        }
     }
 });
