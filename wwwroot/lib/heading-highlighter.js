ba24cfa4426ff0ce840af9a3ea76d335acdd61c8
Caleb Everett <everettcaleb95@gmail.com>
2015-05-25 12:36:10 -0400

    Fixed bug in highlighter that was merging titles rather than doing it for-each

--- wwwroot/lib/heading-highlighter.js
+++ wwwroot/lib/heading-highlighter.js
@@ -3,16 +3,17 @@ define(['jquery'], function($) {
         'a', 'an', 'by', 'on', 'onto', 'the'  
     ];
     
-    var $title = $('article.post').find('h2.title');
-    
-    $title.html($title.text().split(' ').map(function(word) {
-        var lowerWord = word.toLowerCase(), isFiller = false;
-        fillerWords.forEach(function(value, index) {
-            isFiller = isFiller || lowerWord === value;
-        });
-        
-        return "<span class='" +
-            (isFiller ? "filler" : "important") +
-            "'>" + word + "</span>";
-    }).join('&nbsp;'));
+    $('article.post').find('h2.title').each(function(i, title) {
+        var $t = $(title);
+        $t.html($t.text().split(' ').map(function(word) {
+            var lowerWord = word.toLowerCase(), isFiller = false;
+            fillerWords.forEach(function(value, index) {
+                isFiller = isFiller || lowerWord === value;
+            });
+            
+            return "<span class='" +
+                (isFiller ? "filler" : "important") +
+                "'>" + word + "</span>";
+        }).join('&nbsp;'));
+    });
 });
